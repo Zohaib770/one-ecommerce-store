7f69b1dcf593cc97a20316c428af1eaeae2906f4
Zohaib Khalid <kzohaib77@gmail.com>
2025-03-01 11:28:49 +0100

    add routes

--- one-ecommerce-store-ui/src/App.tsx
+++ one-ecommerce-store-ui/src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from "./components/Header.tsx"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/Main.tsx'
 import Shop from './components/Shop.tsx'
-import Footer from './components/Footer.tsx'
+import ProductDetails from './components/ProductDetails.tsx'
+import Cart from './components/Cart.tsx'
+import Checkout from './components/Checkout.tsx'
 
 
 function App() {
@@ -10,22 +11,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-            <>
-              <Header />
-              <Main />
-              <Footer />
-            </>
-          }/>
-        <Route path="/shop" element={
-            <>
-              <Header />
-              <Shop />
-              <Footer />
-            </>
-          }/>
+        <Route path="/" element={<Main />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
       </Routes>
-  </Router>
+    </Router>
   )
 }
 
