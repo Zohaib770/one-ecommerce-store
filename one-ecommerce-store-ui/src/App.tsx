da94c90fbd5189db7754b5a1ae9754d5458e82cd
Zohaib Khalid <kzohaib77@gmail.com>
2025-03-09 06:56:17 +0100

    add legal routes

--- one-ecommerce-store-ui/src/App.tsx
+++ one-ecommerce-store-ui/src/App.tsx
@@ -5,6 +5,12 @@ import ProductDetails from './components/ProductDetails.tsx'
 import Cart from './components/Cart.tsx'
 import Checkout from './components/Checkout.tsx'
 
+import Legal from './components/footer-pages/Legal.tsx'
+import PrivacyPolicy from './components/footer-pages/PrivacyPolicy.tsx'
+import DeliveryPolicy from './components/footer-pages/DeliveryPolicy.tsx'
+import TermsOfService from './components/footer-pages/TermsOfService.tsx'
+import Faqs from './components/footer-pages/Faqs.tsx'
+
 
 function App() {
 
@@ -16,6 +22,14 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/product-details/:id" element={<ProductDetails />} />
+        
+        <Route path="/legal" element={<Legal />}>
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="delivery-policy" element={<DeliveryPolicy />} />
+          <Route path="terms-of-service" element={<TermsOfService />} />
+          <Route path="faqs" element={<Faqs />} />
+        </Route>
+      
       </Routes>
     </Router>
   )
