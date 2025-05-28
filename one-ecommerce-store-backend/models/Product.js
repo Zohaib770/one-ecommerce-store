867ea0b8bcadf00d66cbfe4dd0afb05ed4aa7ddf
Zohaib Khalid <kzohaib77@gmail.com>
2025-05-28 14:07:06 +0500

    update imag to array

--- one-ecommerce-store-backend/models/Product.js
+++ one-ecommerce-store-backend/models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
   name: String,
   description: String,
   price: Number,
-  image: String, // url path
+  image: [String], // url path
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
